a177421f1583fed047aac72975a64e53e7606687
YISEN CAI@win10Panson <yisen614@gmail.com>
2021-08-28 22:46:04 +0800

    return root cause exception message

--- src/main/java/com/glancebar/demo/exceptions/ExceptionsHandler.java
+++ src/main/java/com/glancebar/demo/exceptions/ExceptionsHandler.java
@@ -1,10 +1,14 @@
 package com.glancebar.demo.exceptions;
 
+import org.springframework.core.NestedRuntimeException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +21,9 @@ public class ExceptionsHandler {
 
     @ExceptionHandler(value = RuntimeException.class)
     public ResponseEntity<?> handleRuntimeException(RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        Throwable rootCause = ((NestedRuntimeException) e).getRootCause();
+        // TODO: 2021/8/28 handle null gracefully
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(rootCause != null ? rootCause.getMessage() : null);
     }
 
 
